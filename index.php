98764969bc79cffea3a8839d9c559c66c43b4200
09james <133207216+09james@users.noreply.github.com>
2024-09-29 20:41:36 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -2,80 +2,117 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Register & Login</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-    <div class="container" id="signup" style="display:none;">
-      <h1 class="form-title">Register</h1>
-      <form method="post" action="register.php">
-        <div class="input-group">
-           <i class="fas fa-user"></i>
-           <input type="text" name="fName" id="fName" placeholder="First Name" required>
-           <label for="fname">First Name</label>
+    <title>Lipa na mpesa</title>
+    <link
+      href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+      rel="stylesheet"
+    />
+    <link href="" rel="stylesheet" />
+    <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" ">
+    <script
+      type="text/javascript"
+      src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"
+    ></script>
+    <style>
+      @import url("https://fonts.googleapis.com/css2?family=Rubik:wght@500&display=swap");
+
+      body {
+        background-color: #eaedf4;
+        font-family: "Rubik", sans-serif;
+      }
+
+      .card {
+        width: 310px;
+        border: none;
+        border-radius: 15px;
+      }
+
+      .justify-content-around div {
+        border: none;
+        border-radius: 20px;
+        background: #f3f4f6;
+        padding: 5px 20px 5px;
+        color: #8d9297;
+      }
+
+      .justify-content-around span {
+        font-size: 12px;
+      }
+
+      .justify-content-around div:hover {
+        background: #545ebd;
+        color: #fff;
+        cursor: pointer;
+      }
+
+      .justify-content-around div:nth-child(1) {
+        background: #545ebd;
+        color: #fff;
+      }
+
+      span.mt-0 {
+        color: #8d9297;
+        font-size: 12px;
+      }
+
+      h6 {
+        font-size: 15px;
+      }
+      .mpesa {
+        background-color: green !important;
+      }
+
+      img {
+        border-radius: 15px;
+      }
+    </style>
+  </head>
+  <body oncontextmenu="return false" class="snippet-body">
+    <div class="container d-flex justify-content-center">
+      <div class="card mt-5 px-3 py-4">
+        <div class="d-flex flex-row justify-content-around">
+          <div class="mpesa"><span>Mpesa </span></div>
+          <div><span>Paypal</span></div>
+          <div><span>Card</span></div>
         </div>
-        <div class="input-group">
-            <i class="fas fa-user"></i>
-            <input type="text" name="lName" id="lName" placeholder="Last Name" required>
-            <label for="lName">Last Name</label>
+        <div class="media mt-4 pl-2">
+          <img src="./images/1200px-M-PESA_LOGO-01.svg.png" class="mr-3" height="75" />
+          <div class="media-body">
+            <h6 class="mt-1">Enter Amount & Number</h6>
+          </div>
         </div>
-        <div class="input-group">
-            <i class="fas fa-envelope"></i>
-            <input type="email" name="email" id="email" placeholder="Email" required>
-            <label for="email">Email</label>
+        <div class="media mt-3 pl-2">
+                          <!--bs5 input-->
+
+            <form class="row g-3" action="./stk_initiate.php" method="POST">
+            
+                <div class="col-12">
+                  <label for="inputAddress" class="form-label">Amount</label>
+                  <input type="text" class="form-control" name="amount" placeholder="Enter Amount">
+                </div>
+                <div class="col-12">
+                  <label for="inputAddress2" class="form-label" >Phone Number</label>
+                  <input type="text" class="form-control" name="phone"  placeholder="Enter Phone Number">
+                </div>
+             
+                <div class="col-12">
+                  <button type="submit" class="btn btn-success" name="submit" value="submit">Donate</button>
+                </div>
+              </form>
+              <!--bs5 input-->
+          </div>
         </div>
-        <div class="input-group">
-            <i class="fas fa-lock"></i>
-            <input type="password" name="password" id="password" placeholder="Password" required>
-            <label for="password">Password</label>
-        </div>
-       <input type="submit" class="btn" value="Sign Up" name="signUp">
-      </form>
-      <p class="or">
-        ----------or--------
-      </p>
-      <div class="icons">
-        <i class="fab fa-google"></i>
-        <i class="fab fa-facebook"></i>
-      </div>
-      <div class="links">
-        <p>Already Have Account ?</p>
-        <button id="signInButton">Sign In</button>
       </div>
     </div>
-
-    <div class="container" id="signIn">
-        <h1 class="form-title">Sign In</h1>
-        <form method="post" action="register.php">
-          <div class="input-group">
-              <i class="fas fa-envelope"></i>
-              <input type="email" name="email" id="email" placeholder="Email" required>
-              <label for="email">Email</label>
-          </div>
-          <div class="input-group">
-              <i class="fas fa-lock"></i>
-              <input type="password" name="password" id="password" placeholder="Password" required>
-              <label for="password">Password</label>
-          </div>
-          <p class="recover">
-            <a href="#">Recover Password</a>
-          </p>
-         <input type="submit" class="btn" value="Sign In" name="signIn">
-        </form>
-        <p class="or">
-          ----------or--------
-        </p>
-        <div class="icons">
-          <i class="fab fa-google"></i>
-          <i class="fab fa-facebook"></i>
-        </div>
-        <div class="links">
-          <p>Don't have account yet?</p>
-          <button id="signUpButton">Sign Up</button>
-        </div>
-      </div>
-      <script src="script.js"></script>
-</body>
+    <script
+      type="text/javascript"
+      src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"
+    ></script>
+    <script type="text/javascript" src=""></script>
+    <script type="text/javascript" src=""></script>
+    <script type="text/Javascript"></script>
+  </body>
 </html>
